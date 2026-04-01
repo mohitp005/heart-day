b384bbcdf223fdb9287dd977398fe07d606ceaf8
Mohit Pardeshi <mohitpardeshi111@gmail.com>
2026-04-01 20:18:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const gifStages = [
     "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
     "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
     "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder             
+    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder 
+    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",               // 5 devastated
     "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
     "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif",   // 7 crying runaway
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif"               // 5 devastated
+    
 ]
 
 const noMessages = [
